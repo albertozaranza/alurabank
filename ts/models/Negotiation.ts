438edb5360aecc3eff640c9cc836f278b1a748df
Alberto Zaranza <albertozaranza@outlook.com>
2020-08-08 10:50:37 -0300

    add typescript

---
+++ ts/models/Negotiation.ts
@@ -0,0 +1,27 @@
+class Negotiation {
+  private _date;
+  private _amount;
+  private _value;
+
+  constructor(date, amount, value) {
+    this._date = date;
+    this._amount = amount;
+    this._value = value;
+  }
+
+  get date() {
+    return this._date;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  get volume() {
+    return this._value * this._amount;
+  }
+}
